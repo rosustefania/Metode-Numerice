d08762c4272c69463fac8ba89a7c3aa1e66f7294
Stefania Rosu <56967074+rosustefania@users.noreply.github.com>
2021-06-13 19:29:02 +0300

    Delete Clustering/checker-part-2 directory

--- Clustering/checker-part-2/clustering.m
+++
@@ -1,6 +0,0 @@
-% reads input data from files, outputs points and computed centroids
-function [points centroids] = clustering(file_params, file_points)
-	[NC dims points] = read_input_data(file_params, file_points);
-	centroids = clustering_pc(points, NC);
-endfunction
-
